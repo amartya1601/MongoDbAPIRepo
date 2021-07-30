7c03641ee1e660355637fd13e529e92fdfcbc4b1
Amartya Banerjee <amartya.banerjee@accenture.com>
2021-07-30 12:51:08 +0530

    Add project files.

---
+++ MongoDbAPI/Controllers/StudentController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+using MongoDbAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MongoDbAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        IMongoClient mc = new MongoClient("mongodb://localhost:27017");
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            var db = mc.GetDatabase("studentdb");
+            var collection = db.GetCollection<Student>("student");
+           var res= (IEnumerable<Student>)collection.Find<Student>(x => true).ToList();
+            return res;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MongoDbAPI/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDbAPI.Models
+{
+    public class Student
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id {
+            
+            get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Stud { get; set; }
+
+        public int Age { get; set; }
+    }
+}
